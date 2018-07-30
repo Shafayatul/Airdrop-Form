344dd93d9452814ddf0ba796bb4b8eec31831d23
Md. Shafayatul Haque <shafayat@webencoder.net>
2018-07-30 16:39:01 +0600

    home page design done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,89 @@
-<h1>Welcome</h1>
-<h2><a href="{{ url('auth/google') }}" class="btn btn-primary">Google Login</a></h2>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap Login Form Template</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="{{ URL::asset('assets/bootstrap/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{ URL::asset('assets/font-awesome/css/font-awesome.min.css')}}">
+		<link rel="stylesheet" href="{{ URL::asset('assets/css/form-elements.css')}}">
+        <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css')}}">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="{{ URL::asset('assets/ico/favicon.png')}}">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ URL::asset('assets/ico/apple-touch-icon-144-precomposed.png')}}">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ URL::asset('assets/ico/apple-touch-icon-114-precomposed.png')}}">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ URL::asset('assets/ico/apple-touch-icon-72-precomposed.png')}}">
+        <link rel="apple-touch-icon-precomposed" href="{{ URL::asset('assets/ico/apple-touch-icon-57-precomposed.png')}}">
+
+    </head>
+
+    <body>
+
+        <!-- Top content -->
+        <div class="top-content">
+        	
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 text">
+                            <h1><strong>Point</strong> Login Form</h1>
+                            <div class="description">
+                            	<p>
+	                            	Some text here. Some text here. Some text here. Some text here. Some text here. Some text here. Some text here. Some text here. 
+                            	</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 form-box">
+                        	<div class="form-top">
+                        		<div class="form-top-left">
+                        			<h3>Login to our site</h3>
+                            		<p>You have to use google account to login/signup to our site:</p>
+                        		</div>
+                        		<div class="form-top-right">
+                        			<i class="fa fa-lock"></i>
+                        		</div>
+                            </div>
+                            <div class="form-bottom">
+			                    <a href="{{ url('auth/google') }}">
+									<button type="submit" class="btn"><i class="fa fa-google-plus"></i> Google Plus</button>
+	                        		
+	                        	</a>
+		                    </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
+        </div>
+
+
+        <!-- Javascript -->
+        <script src="{{ URL::asset('assets/js/jquery-1.11.1.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/js/jquery.backstretch.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/js/scripts.js')}}"></script>
+        
+        <!--[if lt IE 10]>
+            <script src="{{ URL::asset('assets/js/placeholder.js')}}"></script>
+        <![endif]-->
+
+    </body>
+
+</html>
