93fed8b4edbf5e63b70fd98d549d2edc90a55316
Md. Shafayatul Haque <shafayat@webencoder.net>
2018-08-02 13:05:53 +0600

    terms and condition checkbox added

--- resources/views/twos/form.blade.php
+++ resources/views/twos/form.blade.php
@@ -12,4 +12,7 @@
 {!! NoCaptcha::renderJs() !!}
 {!! NoCaptcha::display() !!}
 
+<p><input id="field_terms" type="checkbox" required>
+<label for="field_terms">I accept the <u>Terms and Conditions</u></label></p>
+
 {!! Form::submit(isset($submitButtonText) ? $submitButtonText : 'Create', ['class' => 'btn btn-primary form-margin']) !!}
