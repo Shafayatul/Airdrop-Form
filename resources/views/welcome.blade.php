d27c03665c1870af798481bfca49fd573efdfd3e
Md. Shafayatul Haque <shafayat@webencoder.net>
2018-08-07 12:46:59 +0600

    changing the design of home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
         <link rel="stylesheet" href="{{ URL::asset('assets/bootstrap/css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{ URL::asset('assets/font-awesome/css/font-awesome.min.css')}}">
-		<link rel="stylesheet" href="{{ URL::asset('assets/css/form-elements.css')}}">
+        <link rel="stylesheet" href="{{ URL::asset('assets/css/form-elements.css')}}">
         <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css')}}">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -29,45 +29,212 @@
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ URL::asset('assets/ico/apple-touch-icon-72-precomposed.png')}}">
         <link rel="apple-touch-icon-precomposed" href="{{ URL::asset('assets/ico/apple-touch-icon-57-precomposed.png')}}">
 
+        <style type="text/css">
+            body {
+                padding-top: 90px;
+            }
+            .panel-login {
+                border-color: #ccc;
+                -webkit-box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
+                -moz-box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
+                box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
+            }
+            .panel-login>.panel-heading {
+                color: #00415d;
+                background-color: #fff;
+                border-color: #fff;
+                text-align:center;
+            }
+            .panel-login>.panel-heading a{
+                text-decoration: none;
+                color: #666;
+                font-weight: bold;
+                font-size: 15px;
+                -webkit-transition: all 0.1s linear;
+                -moz-transition: all 0.1s linear;
+                transition: all 0.1s linear;
+            }
+            .panel-login>.panel-heading a.active{
+                color: #029f5b;
+                font-size: 18px;
+            }
+            .panel-login>.panel-heading hr{
+                margin-top: 10px;
+                margin-bottom: 0px;
+                clear: both;
+                border: 0;
+                height: 1px;
+                background-image: -webkit-linear-gradient(left,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.15),rgba(0, 0, 0, 0));
+                background-image: -moz-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+                background-image: -ms-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+                background-image: -o-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+            }
+            .panel-login input[type="text"],.panel-login input[type="email"],.panel-login input[type="password"] {
+                height: 45px;
+                border: 1px solid #ddd;
+                font-size: 16px;
+                -webkit-transition: all 0.1s linear;
+                -moz-transition: all 0.1s linear;
+                transition: all 0.1s linear;
+            }
+            .panel-login input:hover,
+            .panel-login input:focus {
+                outline:none;
+                -webkit-box-shadow: none;
+                -moz-box-shadow: none;
+                box-shadow: none;
+                border-color: #ccc;
+            }
+            .btn-login {
+                background-color: #59B2E0;
+                outline: none;
+                color: #fff;
+                font-size: 14px;
+                height: auto;
+                font-weight: normal;
+                padding: 14px 0;
+                text-transform: uppercase;
+                border-color: #59B2E6;
+            }
+            .btn-login:hover,
+            .btn-login:focus {
+                color: #fff;
+                background-color: #53A3CD;
+                border-color: #53A3CD;
+            }
+            .forgot-password {
+                text-decoration: underline;
+                color: #888;
+            }
+            .forgot-password:hover,
+            .forgot-password:focus {
+                text-decoration: underline;
+                color: #666;
+            }
+
+            .btn-register {
+                background-color: #1CB94E;
+                outline: none;
+                color: #fff;
+                font-size: 14px;
+                height: auto;
+                font-weight: normal;
+                padding: 14px 0;
+                text-transform: uppercase;
+                border-color: #1CB94A;
+            }
+            .btn-register:hover,
+            .btn-register:focus {
+                color: #fff;
+                background-color: #1CA347;
+                border-color: #1CA347;
+            }
+        </style>
+
+
+
     </head>
 
     <body>
 
         <!-- Top content -->
         <div class="top-content">
-        	
+            
             <div class="inner-bg">
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-8 col-sm-offset-2 text">
-                            <h1><strong>Point</strong> Login Form</h1>
+                            <h1><strong>Bootstrap</strong> Login Form</h1>
                             <div class="description">
-                            	<p>
-	                            	Some text here. Some text here. Some text here. Some text here. Some text here. Some text here. Some text here. Some text here. 
-                            	</p>
+                                <p>
+                                    This is a free responsive login form made with Bootstrap. 
+                                    Download it on <a href="http://azmind.com"><strong>AZMIND</strong></a>, customize and use it as you like!
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-sm-6 col-sm-offset-3 form-box">
-                        	<div class="form-top">
-                        		<div class="form-top-left">
-                        			<h3>Login to our site</h3>
-                            		<p>You have to use google account to login/signup to our site:</p>
-                        		</div>
-                        		<div class="form-top-right">
-                        			<i class="fa fa-lock"></i>
-                        		</div>
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="panel panel-login">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-6">
+                                            <a href="#" class="active" id="login-form-link">Login</a>
+                                        </div>
+                                        <div class="col-xs-6">
+                                            <a href="#" id="register-form-link">Register</a>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <form id="login-form" action="https://phpoll.com/login/process" method="post" role="form" style="display: block;">
+                                                <div class="form-group">
+                                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                                </div>
+                                                <div class="form-group text-center">
+                                                    <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+                                                    <label for="remember"> Remember Me</label>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="row">
+                                                        <div class="col-sm-6 col-sm-offset-3">
+                                                            <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <div class="text-center">
+                                                                <a href="https://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                            <form id="register-form" action="https://phpoll.com/register/process" method="post" role="form" style="display: none;">
+                                                <div class="form-group">
+                                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="row">
+                                                        <div class="col-sm-6 col-sm-offset-3">
+                                                            <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="form-bottom">
-			                    <a href="{{ url('auth/google') }}">
-									<button type="submit" class="btn"><i class="fa fa-google-plus"></i> Google Plus</button>
-	                        		
-	                        	</a>
-		                    </div>
                         </div>
                     </div>
-
+                    <div class="row">
+                        <div class="col-sm-4 col-sm-offset-4 social-login">
+                            <h3>...or login with:</h3>
+                            <div class="social-login-buttons">
+                                <a class="btn btn-link-2" href="{{ url('auth/google') }}">
+                                    <i class="fa fa-google-plus"></i> Google Plus
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             
@@ -83,6 +250,26 @@
         <!--[if lt IE 10]>
             <script src="{{ URL::asset('assets/js/placeholder.js')}}"></script>
         <![endif]-->
+        <script type="text/javascript">
+            $(function() {
+
+                $('#login-form-link').click(function(e) {
+                    $("#login-form").delay(100).fadeIn(100);
+                    $("#register-form").fadeOut(100);
+                    $('#register-form-link').removeClass('active');
+                    $(this).addClass('active');
+                    e.preventDefault();
+                });
+                $('#register-form-link').click(function(e) {
+                    $("#register-form").delay(100).fadeIn(100);
+                    $("#login-form").fadeOut(100);
+                    $('#login-form-link').removeClass('active');
+                    $(this).addClass('active');
+                    e.preventDefault();
+                });
+
+            });
+        </script>
 
     </body>
 
